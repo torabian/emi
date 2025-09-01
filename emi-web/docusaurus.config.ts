4965ccd779ba35c72839929f7b7ee35966223f97
Ali Torabi <torabian@users.noreply.github.com>
2025-09-01 22:19:31 +0200

    Fix the delpoy (#7)
    Co-authored-by: Ali <Ali>

--- emi-web/docusaurus.config.ts
+++ emi-web/docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/emi/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "github.com/torabian", // Usually your GitHub org/user name.
+  projectName: "Emi", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
