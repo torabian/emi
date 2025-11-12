37879e339aeb8345d88cb6f375829332d6a8b69e
Ali <Ali>
2025-11-12 13:18:37 +0100

    U

--- tests/virtual-eco-system/go/sdkgen/GiantDto.go
+++ tests/virtual-eco-system/go/sdkgen/GiantDto.go
@@ -1,11 +1,8 @@
 package unknownpackage
-
-import "github.com/torabian/emi/emigo"
-
-// The base class definition for giantDto
+  // The base class definition for giantDto
 type GiantDto struct {
-	FirstName         string                                `json:"firstName" yaml:"firstName"`
-	FirstNameNullable emigo.Nullable[string]                `json:"firstNameNullable" yaml:"firstNameNullable"`
-	Array             []GiantDtoArray                       `json:"array" yaml:"array"`
-	ArrayNullable     emi.Nullable[[]GiantDtoArrayNullable] `json:"arrayNullable" yaml:"arrayNullable"`
+		FirstName string `json:"firstName" yaml:"firstName"`
+		FirstNameNullable emigo.Nullable[string] `json:"firstNameNullable" yaml:"firstNameNullable"`
+		Array []GiantDtoArray `json:"array" yaml:"array"`
+		ArrayNullable emi.Nullable[[]GiantDtoArrayNullable] `json:"arrayNullable" yaml:"arrayNullable"`
 }
--- tests/virtual-eco-system/kotlin/app/src/main/kotlin/org/example/sdkgen/ModifyGiantDtoAction.kt
+++ tests/virtual-eco-system/kotlin/app/src/main/kotlin/org/example/sdkgen/ModifyGiantDtoAction.kt
@@ -3,9 +3,9 @@ import okhttp3.MediaType.Companion.toMediaType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import okhttp3.*
 import okhttp3.RequestBody.Companion.toRequestBody
-import kotlinx.serialization.json.*
 import GiantDto "./GiantDto"
 /**
  * Action to communicate with the action ModifyGiantDtoAction
